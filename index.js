bdba54dc1b0d2b3032f385e07eaa1316c5060a08
jesse_robertson <jesse.d.robertson@gmail.com>
2016-04-05 20:09:43 +0000

    Server cleanup and documentation

--- index.js
+++ index.js
@@ -3,22 +3,39 @@ var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 
+// Define socket event constants
+var BOUNCE_EVENT     = 'boing',
+    LOSS_EVENT       = 'lost-ball',
+    DISCONNECT_EVENT = 'disconnect';
+
+// Make socket.io default to websockets
 io.set('transports', ['websocket']);
 
+// Serve public/ folder as static assets
 app.use(express.static('public'));
 
+// Start express server
 server.listen(process.env.PORT, process.env.IP);
 
+// Respond to incoming socket connections and data
 io.on('connection', function (socket) {
-    socket.on('boing', function(data) {
-        socket.broadcast.emit('boing', {
+    
+    // When a client sends a bounce event...
+    socket.on(BOUNCE_EVENT, function(data) {
+        
+        // ...broadcast the event to the other players
+        socket.broadcast.emit(BOUNCE_EVENT, {
             id: socket.id,
             position : data.position,
             velocity : data.velocity
         });
     });
-    socket.on('disconnect', function() {
-        socket.broadcast.emit('lost-ball', {
+    
+    // When a client disconnects...
+    socket.on(DISCONNECT_EVENT, function() {
+        
+        // ...broadcast a loss event to the other players 
+        socket.broadcast.emit(LOSS_EVENT, {
             id : socket.id
         });
     });
